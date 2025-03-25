59cfea6695d50a3f88850eab301eb29436abea91
mirabellachn <mirabellacn@gmail.com>
2025-03-26 00:45:13 +0700

    Initial Commit

---
+++ 9to5App/_to5AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _to5AppApp.swift
+//  9to5App
+//
+//  Created by Mirabella on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct _to5AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
