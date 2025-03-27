d1b42d97230a02ab9961ada8dabe8601bac6d895
Revanza Narendra <revanzanrndra@gmail.com>
2025-03-27 17:12:23 +0700

    apalah

---
+++ 9to5App/Apalah.swift
@@ -0,0 +1,18 @@
+//
+//  Apalah.swift
+//  9to5App
+//
+//  Created by Tm Revanza Narendra Pradipta on 27/03/25.
+//
+
+import SwiftUI
+
+struct Apalah: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Apalah()
+}
